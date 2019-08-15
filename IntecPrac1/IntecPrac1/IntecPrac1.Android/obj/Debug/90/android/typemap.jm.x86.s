8f626194f5616350cab810e507e988afd1367907
emysaul <emysaul@hotmail.es>
2019-08-15 11:09:22 -0400

    proyect added

---
+++ IntecPrac1/IntecPrac1/IntecPrac1.Android/obj/Debug/90/android/typemap.jm.x86.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: 100a98133e77aa668ecd441e1e20a452346f3c7f */
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1343
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	117
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 351867
+	.include	"typemap.jm.inc"
---
+++ IntecPrac1/IntecPrac1/IntecPrac1/Login.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace IntecPrac1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        public async void OnValidate(object sender, EventArgs e)
+        {
+            bool isEmail = !string.IsNullOrEmpty(txtEmail.Text) && Regex.IsMatch(txtEmail.Text, @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z", RegexOptions.IgnoreCase);
+
+            bool isPassword = !string.IsNullOrEmpty(txtPassword.Text);
+
+
+            if (isEmail && isPassword)
+                await DisplayAlert("Alert", $"Hola {txtEmail.Text.Substring(0, txtEmail.Text.IndexOf('@'))}", "Ok");
+            else if (!isEmail)
+                await DisplayAlert("Alert", $"Campo email invalido", "Ok");
+            else if (!isPassword)
+                await DisplayAlert("Alert", $"Password no puede estar vacio", "Ok");
+
+
+
+        }
+    }
+}
